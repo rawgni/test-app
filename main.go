c3291a670a4463329dc364ffcf2d24fa5a7d7c41
rawgni <ingwar.wirjawan@gmail.com>
2019-09-24 10:45:47 +0700

    testing

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+
+)
 
 func main() {
 	x := make(map[int]int, 0)
 	x[5] = 4
 	fmt.Printf("hello world")
 	fmt.Println(x)
+
+	log.WithFields(log.Fields{ "animal": "walrus" }).Info("A walrus appears")
 }
